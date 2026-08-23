f2df468acd428ecf1975514302c30c8a3c373c85
Shahab <shahabkadkhodai-khalfi@hotmail.com>
2026-08-23 10:20:50 +0100

    Separated the C++ PG code into distinct functions

--- Rejection-Sampling-PG.cpp
+++ Rejection-Sampling-PG.cpp
@@ -42,12 +42,7 @@ unsigned long long factorial(unsigned long long number) {
     return total;
 }
 
-int main() {
-
-    cout << "\nRejection Sampling Permutation Generator (C++)\n";
-    cout << "----------------------------------------------\n";
-
-    string word;
+void input_from_user(string &word) {
 
     while (true) {
 
@@ -63,8 +58,80 @@ int main() {
             break;
         }
     }
+}
 
-    // Remove trailing and leading spaces from word later
+void generating_word (
+    string &word,
+    vector<int> &indexes,
+    vector<char> &unique_keys_copy,
+    unordered_map<char, int> &letters_copy,
+    vector<char> &generated_vector
+    ) {
+
+    int random_index = 0;
+    char random_letter = '\0';
+    unsigned long long number_pos = 0;
+    unsigned long long letter_pos = 0;
+
+    for (int counter = 0; counter < word.length(); counter++) {
+
+        number_pos = random(0, (indexes.size() - 1));
+        random_index = indexes.at(number_pos);
+        indexes.erase(indexes.begin() + number_pos);
+
+        letter_pos = random(0, (unique_keys_copy.size() - 1));
+        random_letter = unique_keys_copy.at(letter_pos);
+        letters_copy.at(random_letter) -= 1;
+
+        if (letters_copy.at(random_letter) == 0) {
+            letters_copy.erase(random_letter);
+            unique_keys_copy.erase(unique_keys_copy.begin() + letter_pos);
+        }
+
+        generated_vector.at(random_index) = random_letter;
+    }
+}
+
+void permutation_generator (
+    unordered_set<string> &permutations,
+    const unsigned long long &no_of_combinations,
+    unordered_map<char, int> &letters,
+    vector<char> &unique_keys,
+    string &word
+    ) {
+
+    while (permutations.size() < no_of_combinations) {
+
+        unordered_map<char, int> letters_copy = letters;
+        vector<char> unique_keys_copy = unique_keys;
+        vector<char> generated_vector = {};
+        string generated_word;
+        vector<int> indexes = {};
+
+        for (int i = 0; i < word.length(); i++) {
+            generated_vector.push_back('\0');
+            indexes.push_back(i);
+        }
+
+        generating_word(word, indexes, unique_keys_copy, letters_copy, generated_vector);
+
+        for (char letter : generated_vector) {
+            generated_word += letter;
+        }
+
+        permutations.insert(generated_word);
+    }
+}
+
+int main() {
+
+    cout << "\nRejection Sampling Permutation Generator (C++)\n";
+    cout << "----------------------------------------------\n";
+
+    string word;
+
+    input_from_user(word);
+
     unordered_set<string> permutations = {word};
     unordered_map<char, int> letters;
     vector<char> unique_keys = {};
@@ -84,51 +151,11 @@ int main() {
         denominator *= factorial(value.second);
     }
 
-    unsigned long long no_of_combinations = factorial( word.length()) / denominator;
+    const unsigned long long no_of_combinations = factorial( word.length()) / denominator;
 
     cout << "This word has " + to_string(no_of_combinations) + " combination(s).\n";
 
-
-    while (permutations.size() < no_of_combinations) {
-        unordered_map<char, int> letters_copy = letters;
-        vector<char> unique_keys_copy = unique_keys;
-        vector<char> generated_vector = {};
-        string generated_word;
-        vector<int> indexes = {};
-        int random_index = 0;
-        char random_letter = '\0';
-        long long number_pos = 0;
-        unsigned long long letter_pos = 0;
-
-        for (int i = 0; i < word.length(); i++) {
-            generated_vector.push_back('\0');
-            indexes.push_back(i);
-        }
-
-        for (int counter = 0; counter < word.length(); counter++) {
-
-            number_pos = random(0, (indexes.size() - 1));
-            random_index = indexes.at(number_pos);
-            indexes.erase(indexes.begin() + number_pos);
-
-            letter_pos = random(0, (unique_keys_copy.size() - 1));
-            random_letter = unique_keys_copy.at(letter_pos);
-            letters_copy.at(random_letter) -= 1;
-
-            if (letters_copy.at(random_letter) == 0) {
-                letters_copy.erase(random_letter);
-                unique_keys_copy.erase(unique_keys_copy.begin() + letter_pos);
-            }
-
-            generated_vector.at(random_index) = random_letter;
-        }
-
-        for (char letter : generated_vector) {
-            generated_word += letter;
-        }
-
-        permutations.insert(generated_word);
-    }
+    permutation_generator(permutations, no_of_combinations, letters, unique_keys, word);
 
     cout << "\nAll permutations:\n";
 
