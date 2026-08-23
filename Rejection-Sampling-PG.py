d0f87f394ea66e39dade7f93bc16821892cb1e33
Shahab <shahabkadkhodai-khalfi@hotmail.com>
2026-08-23 23:51:34 +0100

    Added type hints to all variables and function return types to the Python PG

--- Rejection-Sampling-PG.py
+++ Rejection-Sampling-PG.py
@@ -1,10 +1,10 @@
 import random
 import math
 
-def input_from_user():
+def input_from_user() -> str:
 
     while True: # Continues looping until valid word is retrieved from user
-        word = input("\nEnter a word (all characters are acceptable): ")
+        word : str = input("\nEnter a word (all characters are acceptable): ")
         word = word.strip()
 
         if len(word) == 0:
@@ -15,34 +15,38 @@ def input_from_user():
 
     return word
 
-def permutationGenerator(permutations, no_of_combinations, letters, word):
+def permutation_generator (
+        permutations : set[str],
+        no_of_combinations : int,
+        letters : dict[str, int],
+        word : str
+        ) -> None:
 
     while len(permutations) < no_of_combinations: # Begin rejection sampling loop
 
-        letters_copy = letters.copy()
-        generated_word = []
-
-        for i in range(len(word)): # Initialise generated_word list with placeholder values
-            generated_word.append(None)
+        letters_copy : dict[str, int] = letters.copy()
+        generated_word : list[str] = [""] * len(word) # Initialise generated_word list with placeholder empty strings
 
         generating_word(generated_word, letters_copy, word)
 
-        generated_word = "".join(generated_word) # Convert the list to a string
+        permutations.add("".join(generated_word)) # Convert the generated_word list to a string and append the string to permutations
 
-        permutations.add(generated_word)
+def generating_word (
+        generated_word : list[str],
+        letters_copy : dict[str, int],
+        word : str
+        ) -> None:
 
-def generating_word(generated_word, letters_copy, word):
+    used_indexes : list[int] = []
 
-    index_exceptions = []
-
-    for counter in range(len(word)):
+    for letter in word:
     
         # Find a random location to place a letter in the generated_word list
-        random_index = random.choice([i for i in range(0, len(word)) if i not in index_exceptions])
-        index_exceptions.append(random_index)
+        random_index : int = random.choice([i for i in range(0, len(word)) if i not in used_indexes])
+        used_indexes.append(random_index)
 
         # Select a random letter from the dictionary to place into the generated_word list
-        random_letter = random.choice([key for key in letters_copy.keys()])
+        random_letter : str = random.choice([key for key in letters_copy.keys()])
         letters_copy[random_letter] -= 1
     
         if letters_copy[random_letter] == 0: # Remove character from letters_copy if its value is 0
@@ -54,11 +58,11 @@ def main():
 
     print("\n\033[4mRejection Sampling Permutation Generator (Python)\033[0m")
 
-    word = input_from_user()
+    word : str = input_from_user()
 
-    permutations = {word} # Permutations are served in a set which automatically handle repeated values
-    letters = dict() # All letters within the user's word are handled through a dictionary (map)
-    denominator = 1
+    permutations : set[str] = {word} # Permutations are served in a set which automatically handle repeated values
+    letters : dict[str, int] = dict() # All letters within the user's word are handled through a dictionary (map)
+    denominator : int = 1
 
     for letter in word:
         if letter not in letters: # Add a new entry to letters if one does not exist
@@ -69,11 +73,11 @@ def main():
     for value in letters.values(): # Loop through all values in letters to account for characters that appear more that once and update the denominator accordingly
         denominator *= math.factorial(value)
 
-    no_of_combinations = math.factorial(len(word)) / denominator # Calculate number of permutations
+    no_of_combinations : int = math.factorial(len(word)) // denominator # Calculate number of permutations
 
     print(f"\nThis word has {no_of_combinations} combination(s).")
 
-    permutationGenerator(permutations, no_of_combinations, letters, word)
+    permutation_generator(permutations, no_of_combinations, letters, word)
 
     print(f"\nAll permutations:\n {permutations}")
 
